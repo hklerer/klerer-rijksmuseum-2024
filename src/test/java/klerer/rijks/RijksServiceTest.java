9103b96c67d65bca8739ff91c84d1d287de78f2f
hannahklerer <hklerer@student.touro.edu>
2024-05-14 13:09:59 -0400

    fixed apikey

--- src/test/java/klerer/rijks/RijksServiceTest.java
+++ src/test/java/klerer/rijks/RijksServiceTest.java
@@ -1,5 +1,6 @@
 package klerer.rijks;
 
+import com.andrewoid.ApiKey;
 import klerer.rijks.json.RijksCollection;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +11,11 @@ class RijksServiceTest {
     public void pageNum() {
         // given
         ApiKey apiKey = new ApiKey();
+        String keyString = apiKey.get();
         RijksService service = new RijksServiceFactory().getService();
 
         // when
-        RijksCollection rijksCollection = service.pageNum(apiKey.toString(), 1).blockingGet();
+        RijksCollection rijksCollection = service.pageNum(keyString, 1).blockingGet();
 
         // then
         assertNotNull(rijksCollection.artObjects[0].title);
@@ -26,10 +28,11 @@ class RijksServiceTest {
     public void queryAndPageNum() {
         // given
         ApiKey apiKey = new ApiKey();
+        String keyString = apiKey.get();
         RijksService service = new RijksServiceFactory().getService();
 
         // when
-        RijksCollection rijksCollection = service.queryAndPageNum(apiKey.toString(),
+        RijksCollection rijksCollection = service.queryAndPageNum(keyString,
                 "query",
                 1).blockingGet();
 
@@ -44,10 +47,11 @@ class RijksServiceTest {
     public void involvedMakerAndPageNum() {
         // given
         ApiKey apiKey = new ApiKey();
+        String keyString = apiKey.get();
         RijksService service = new RijksServiceFactory().getService();
 
         // when
-        RijksCollection rijksCollection = service.involvedMakerAndPageNum(apiKey.toString(),
+        RijksCollection rijksCollection = service.involvedMakerAndPageNum(keyString,
                 "Leonardo Da Vinci",
                 1).blockingGet();
 
