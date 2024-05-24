9cc8746749f5376601e3d71abecbcdc8e4ddd4fc
hannahklerer <hklerer@student.touro.edu>
2024-05-23 22:56:34 -0400

    added to rijks search frame

--- src/main/java/klerer/rijks/RijksSearchFrame.java
+++ src/main/java/klerer/rijks/RijksSearchFrame.java
@@ -1,7 +1,16 @@
 package klerer.rijks;
 
+import com.andrewoid.ApiKey;
+import hu.akarnokd.rxjava3.swing.SwingSchedulers;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import klerer.rijks.json.RijksCollection;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class RijksSearchFrame extends JFrame {
     private RijksService rijksService;
@@ -9,6 +18,10 @@ public class RijksSearchFrame extends JFrame {
     private JButton prevButton;
     private JButton nextButton;
     private JPanel imagePanel;
+    private int currentPage = 1;
+    ApiKey apiKey = new ApiKey();
+    String keyString = apiKey.get();
+    private Disposable disposable;
 
     public RijksSearchFrame() {
         setTitle("Rijks Museum");
@@ -25,10 +38,68 @@ public class RijksSearchFrame extends JFrame {
         top.add(nextButton);
         add(top, BorderLayout.NORTH);
 
-        imagePanel = new JPanel(new GridLayout(5, 5, 5, 5));
+        imagePanel = new JPanel(new GridLayout(2, 5, 5, 5));
         add(new JScrollPane(imagePanel), BorderLayout.CENTER);
+
+        rijksService = new RijksServiceFactory().getService();
+
+        prevButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (currentPage > 1) {
+                    currentPage--;
+                    performSearch();
+                }
+            }
+        });
+
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentPage++;
+                performSearch();
+            }
+        });
+
+        searchBar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentPage = 1;
+                performSearch();
+            }
+        });
+
+        performSearch();
     }
 
+    private void performSearch() {
+
+        if (searchBar.getText().isEmpty()) {
+            disposable = rijksService.pageNum(keyString, currentPage)
+                    // tells Rx to request the data on a background Thread
+                    .subscribeOn(Schedulers.io())
+                    // tells Rx to handle the response on Swing's main Thread
+                    .observeOn(SwingSchedulers.edt())
+                    //.observeOn(AndroidSchedulers.mainThread()) // Instead use this on Android only
+                    .subscribe(
+                            (response) -> handleResponse(response),
+                            Throwable::printStackTrace);
+        } else {
+            disposable = rijksService.queryAndPageNum(keyString, searchBar.getText(), currentPage)
+                    // tells Rx to request the data on a background Thread
+                    .subscribeOn(Schedulers.io())
+                    // tells Rx to handle the response on Swing's main Thread
+                    .observeOn(SwingSchedulers.edt())
+                    //.observeOn(AndroidSchedulers.mainThread()) // Instead use this on Android only
+                    .subscribe(
+                            (response) -> handleResponse(response),
+                            Throwable::printStackTrace);
+        }
+    }
+
+    private void handleResponse(RijksCollection rijksCollection) {
+        imagePanel.removeAll();
+    }
     public static void main(String[] args) {
         RijksSearchFrame frame = new RijksSearchFrame();
         frame.setVisible(true);
